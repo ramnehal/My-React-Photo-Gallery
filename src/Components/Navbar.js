66bdef0694e9944047a1eda2a069f1e879aea7fd
NARIN <narinramnehal21@gmail.com>
2022-05-16 20:04:27 -0400

    Added Navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import logo from '../logo.svg';
+import "../App.css";
+
+function Navbar() {
+    return (
+        <div className="Navbar"> 
+            <div className="leftSide"> 
+                <div className="logo">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+            </div>
+
+            <div className="center">
+                <div className="links">
+                    <a href="/home">Home</a>
+                    <a href="/gallery">Gallery</a>
+                    <a href="/contactUs">Contact Us</a>
+                    <a href="/aboutUs">About Us</a>
+                </div>
+            </div>
+
+            <div className="rightSide">
+                <input type="text" placeholder="Search..." />
+                <button>
+                    <SearchIcon style={{ color: 'white' }} />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
