c2e6466c0568601113cff9eac859ac21e0da92c0
NARIN <narinramnehal21@gmail.com>
2022-05-17 23:49:49 -0400

    Added Logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-import logo from '../logo.svg';
+import logo from '../logo.jpg';
 import "../App.css";
 
 function Navbar() {
