4805f84bd0dfd86f8289fd893c1deba337a05f5a
NARIN <narinramnehal21@gmail.com>
2022-05-22 21:59:21 -0400

    Added filter buttons

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import SearchIcon from '@mui/icons-material/Search';
 import Navbar from './Components/Navbar';
 import Gallery from './Gallery';
+import Filter from './Filter';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Filter />
         <Gallery />
     </div>
   );
---
+++ src/Filter.js
@@ -0,0 +1,13 @@
+import {useEffect} from "react";
+
+function Filter() {
+    return(
+        <div className="filter-container">
+            <button>Button1</button>
+            <button>Button2</button>
+            <button>Button3</button>
+        </div>
+    );
+}
+
+export default Filter;
